db28df7a7b329bb356a16a98a1ada8523db2092d
Wouter Raateland <wouterraateland@gmail.com>
2017-09-07 23:00:41 +0200

    Update public manifest, add logos and names

--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -7,7 +7,7 @@ import { getProductByURL } from 'selectors/products'
 const Product = ({ product: { backgroundColor, color, image, name, price } }) => (
   <div className="ProductPage">
     <div className="ProductPage-header" style={{ backgroundColor }}>
-      <img src={image} />
+      <img src={image} alt={name} />
     </div>
     <div className="ProductPage-body">
       <h2>{name}</h2>
